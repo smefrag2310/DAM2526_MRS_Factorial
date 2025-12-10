9ccee693a1b74bffc5927c206e6e3c14061d4cbf
smefrag2310 <smefrag2310@g.educaand.es>
2025-12-10 09:36:08 +0100

    Quinto commit de la rama main: Versión E
    Se ha añadido la función factorialBucle para calcular el factorial del
    número introduzca el usuario

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -3,42 +3,48 @@ package pruebaControlVersiones;
 import java.util.Scanner;
 
 public class Factorial {
-	
-	 private static Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int num;
-        num=pedirNumero();
+        int num = pedirNumero(sc);
 
         if (num < 0) {
             System.out.println("ERROR: el número debe ser mayor o igual que 0.");
         } else {
             System.out.println("Has introducido el número: " + num);
+            long resultado = factorialBucle(num);
+            System.out.println("RESULTADO - El factorial de " + num + " es: " + resultado);
         }
 
         sc.close();
-        System.out.println("RESULTADO - En versiones posteriores se calculará el factorial del número " + num);
-	}
-	
-	private static int pedirNumero() {
-	    int num;
-	    boolean valido;
+    }
 
-	    do {
-	        System.out.print("Introduce un número entero mayor o igual que 0: ");
-	        if (sc.hasNextInt()) {
-	            num = sc.nextInt();
-	            valido = true;
-	        } else {
-	            System.out.println("Entrada no válida. Introduce un entero.");
-	            sc.next();
-	            valido = false;
-	            num = -1;
-	        }
-	    } while (!valido);
-	    return num;
-	}
+    private static int pedirNumero(Scanner sc) {
+        int num;
+        boolean valido;
 
+        do {
+            System.out.print("Introduce un número entero mayor o igual que 0: ");
+            if (sc.hasNextInt()) {
+                num = sc.nextInt();
+                valido = true;
+            } else {
+                System.out.println("Entrada no válida. Introduce un entero.");
+                sc.next(); // descarta la entrada incorrecta
+                valido = false;
+                num = -1;
+            }
+        } while (!valido);
+
+        return num;
+    }
+
+    private static long factorialBucle(int n) {
+        long resultado = 1;
+        for (int i = 1; i <= n; i++) {
+            resultado *= i;
+        }
+        return resultado;
+    }
 }
 
