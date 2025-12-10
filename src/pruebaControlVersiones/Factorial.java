027d934d5fb099fa6fe74929e884ce651b0310fa
smefrag2310 <smefrag2310@g.educaand.es>
2025-12-10 09:32:05 +0100

    Cuarto commit de la rama main: Versión D
    Mejorado el método pedirNumero para que no sea posible introducirle
    valores que no sean enteros.

--- src/pruebaControlVersiones/Factorial.java
+++ src/pruebaControlVersiones/Factorial.java
@@ -22,8 +22,22 @@ public class Factorial {
 	}
 	
 	private static int pedirNumero() {
-	    System.out.print("Introduce un número entero mayor o igual que 0: ");
-	    return sc.nextInt();
+	    int num;
+	    boolean valido;
+
+	    do {
+	        System.out.print("Introduce un número entero mayor o igual que 0: ");
+	        if (sc.hasNextInt()) {
+	            num = sc.nextInt();
+	            valido = true;
+	        } else {
+	            System.out.println("Entrada no válida. Introduce un entero.");
+	            sc.next();
+	            valido = false;
+	            num = -1;
+	        }
+	    } while (!valido);
+	    return num;
 	}
 
 }
